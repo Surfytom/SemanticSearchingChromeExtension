7e841dad4d0cc4443420f274ff26a4303fdb5502
Surfytom <surfytom@gmail.com>
2023-04-08 04:29:25 +0100

    initial commit

--- src/background.js
+++ src/background.js
@@ -1,21 +1,25 @@
 'use strict';
+var spawner = require('child_process').spawn;
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("background.js installed!")
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+  chrome.contextMenus.create({
+      id: "test",
+      title: "This is a test",
+      type: "normal",
+      contexts: ["selection"]
+  })
+})
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
-});
+chrome.contextMenus.onClicked.addListener(function(info, tab){
+
+  console.log("info: ")
+  console.log(info.selectionText)
+
+  const pythonProcess = spawner('python', ['C:/Users/surfy/Documents/Dissertation/Python Files/BERTopic/test.py', "hi 1"]);
+
+  pythonProcess.stdout.on('data', (data) => {
+      console.log('Data received from python: ', data.toString());
+  })
+})
