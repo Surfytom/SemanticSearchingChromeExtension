d79d1de68be0e81f7d1b630d9ec7d4c0405dd33d
Surfytom <surfytom@gmail.com>
2023-04-12 17:28:07 +0100

    committing

--- src/background.js
+++ src/background.js
@@ -1,5 +1,7 @@
 'use strict';
 var spawner = require('child_process').spawn;
+let processing = false;
+const port = chrome.runtime.connectNative("application");
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log("background.js installed!")
@@ -11,6 +13,22 @@ chrome.runtime.onInstalled.addListener(() => {
       contexts: ["selection"]
   })
 })
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  port.onMessage.addListener((response) => {
+    console.log("Received: " + response);
+    sendResponse({ value: response });
+    processing = false;
+  });
+
+  if (processing) return;
+  console.log("hostname=");
+  console.log(message)
+  processing = true;
+  port.postMessage(message);
+
+  return true;
+});
 
 chrome.contextMenus.onClicked.addListener(function(info, tab){
 
