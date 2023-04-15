13b12b839cd7d45efbd9ea1d19463a40af9548a8
Surfytom <surfytom@gmail.com>
2023-04-15 17:02:48 +0100

    New folder is created for each new session and pdf links work apart from when there is no pdf available

--- src/background.js
+++ src/background.js
@@ -1,6 +1,9 @@
 'use strict';
 
-let connection;
+var connection;
+var bookmarkParent;
+var workingFolderId;
+var sessionID;
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log("background.js installed!")
@@ -10,7 +13,34 @@ chrome.runtime.onInstalled.addListener(() => {
 
   // Add event listener to run when messages are received from the server
   connection.addEventListener("message", (event) => {
-    console.log(event.data)
+    let data = JSON.parse(event.data)
+    console.log(data.response)
+
+    chrome.bookmarks.getChildren(
+      workingFolderId,
+      function(result){
+        console.log("Paper Folder Children:")
+        console.log(result)
+        sessionID = result.length
+
+        let title = "Session " + sessionID
+
+        chrome.bookmarks.create(
+          {'parentId': workingFolderId, 'title': title},
+          function(folder) {
+            console.log("Created new folder for session called: " + folder.title)
+            
+            for (let i = 0; i < data.response.length; i++) {
+              chrome.bookmarks.create({
+                'parentId': folder.id,
+                'title': data.response[i].title,
+                'url': data.response[i].pdfId
+              })
+            }
+          }
+        )
+      }
+    )
   })
 
   // Check a folder has been created for the paper bookmarks to store to if not create it
@@ -19,20 +49,23 @@ chrome.runtime.onInstalled.addListener(() => {
 
       console.log(bookmarkTree[0].children[0])
 
-      let bookmarkBar = bookmarkTree[0].children[0]
+      bookmarkParent = bookmarkTree[0].children[0]
 
       let folderFound = false
-      for (let child in bookmarkBar.children) {
-        if (child.title == "Papers Found - Chrome Extension") {
+      for (let i = 0; i < bookmarkParent.children.length; i++) {
+        if (bookmarkParent.children[i].title == "Papers Found - Chrome Extension") {
           folderFound = true
+          workingFolderId = bookmarkParent.children[i].id
         }
       }
 
       if(!folderFound) {
         chrome.bookmarks.create(
-          {'parentId': bookmarkBar.id, 'title': "Papers Found - Chrome Extension"},
+          {'parentId': bookmarkParent.id, 'title': "Papers Found - Chrome Extension"},
           function(folder) {
-            console.log("New folder created on bookmarks bar: " + folder.title)
+            console.log("New folder created in bookmarks bar: " + folder.title)
+            workingFolderId = folder.id
+            sessionID = 0
           }
         )
       }
@@ -40,7 +73,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
   chrome.contextMenus.create({
       id: "test",
-      title: "This is a test",
+      title: "Scan for related papers",
       type: "normal",
       contexts: ["selection"]
   })
