ca1c05b5886aa2594a942cffb957f1829a17f233
Surfytom <surfytom@gmail.com>
2023-04-15 15:27:27 +0100

    bookmark folder is now created on startup of chrome extension

--- src/background.js
+++ src/background.js
@@ -5,13 +5,38 @@ let connection;
 chrome.runtime.onInstalled.addListener(() => {
   console.log("background.js installed!")
 
-  connection = new WebSocket("ws://localhost:8080/")
+  // Connect to backend server to fetch db data
+  connection = new WebSocket("ws://localhost:7070/")
 
-  //connection.send("hello")
-
+  // Add event listener to run when messages are received from the server
   connection.addEventListener("message", (event) => {
     console.log(event.data)
   })
+
+  // Check a folder has been created for the paper bookmarks to store to if not create it
+  chrome.bookmarks.getTree(
+    function(bookmarkTree){
+
+      console.log(bookmarkTree[0].children[0])
+
+      let bookmarkBar = bookmarkTree[0].children[0]
+
+      let folderFound = false
+      for (let child in bookmarkBar.children) {
+        if (child.title == "Papers Found - Chrome Extension") {
+          folderFound = true
+        }
+      }
+
+      if(!folderFound) {
+        chrome.bookmarks.create(
+          {'parentId': bookmarkBar.id, 'title': "Papers Found - Chrome Extension"},
+          function(folder) {
+            console.log("New folder created on bookmarks bar: " + folder.title)
+          }
+        )
+      }
+    })
 
   chrome.contextMenus.create({
       id: "test",
