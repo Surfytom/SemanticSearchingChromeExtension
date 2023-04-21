e5889e0eb7eeb2f0eaa2cd4fad2542673407b69e
Surfytom <surfytom@gmail.com>
2023-04-21 21:15:52 +0100

    fixed server to return properly. fixed all bugs relating to storing and updating storage values. allows you to send across with limit amount for querying database

--- server/server.js
+++ server/server.js
@@ -1,26 +1,65 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
+const weaviate = require('weaviate-ts-client');
 
 const wss = new WebSocket.Server({ server:server });
 
-var clients = new Map();
+const databaseClient = weaviate.client({
+    scheme: 'http',
+    host: 'localhost:8080',
+});
 
 wss.on('connection', (ws) => {
     console.log('A new client Connected!');
 
-    var sessionID = null;
-
-    ws.on('message', (message) => {
+    ws.on('message', async (message) => {
 
         var data = JSON.parse(message);
         console.log(data);
 
+        responseBody = await queryDatabase(data.body, data.amount)
+        response = JSON.stringify({response: responseBody})
 
+        ws.send(response)
     });
 });
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 server.listen(3000, () => console.log(`Lisening on port :3000`));
+
+async function queryDatabase(queryBody, limitAmount) {
+
+    nearTextArgs = {
+        "concepts": [queryBody]
+    }
+
+    result = await databaseClient.graphql.get().withClassName("Paper").withFields("pdfId title").withNearText(nearTextArgs).withLimit(limitAmount).do()
+
+    result = result.data.Get.Paper
+
+    for (let i = 0; i < result.length; i++) {
+        result[i].pdfId = await convertPdfLink(result[i].pdfId)
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        console.log(result[i])
+    }
+
+    return result
+}
+
+async function convertPdfLink(pdfId){
+
+    resultPdfLink = pdfId
+
+    if (!pdfId.includes("/")) {
+        resultPdfLink = pdfId.replace("-", ".")
+    }
+
+    resultPdfLink = "https://arxiv.org/pdf/" + resultPdfLink + ".pdf"
+
+    return resultPdfLink
+}
--- src/background.js
+++ src/background.js
@@ -3,10 +3,11 @@
 let connection;
 let bookmarkParent;
 let workingFolderId;
-let sessionID;
+let sessionID = 0;
 let workingFolderName;
+let amountOfPapers;
 
-chrome.runtime.onInstalled.addListener(async () => {
+chrome.runtime.onInstalled.addListener(() => {
   console.log("background.js installed!")
 
   // Connect to backend server to fetch db data
@@ -14,72 +15,81 @@ chrome.runtime.onInstalled.addListener(async () => {
 
   // Add event listener to run when messages are received from the server
   connection.addEventListener("message", (event) => {
+
     let data = JSON.parse(event.data)
     console.log(data.response)
 
-    chrome.bookmarks.getChildren(
-      workingFolderId,
-      function(result){
-        console.log("Paper Folder Children:")
-        console.log(result)
-        sessionID = result.length
+    chrome.bookmarks.getChildren(workingFolderId).then((result) => {
+      console.log("Paper Folder Children:")
+      console.log(result)
 
-        let title = "Session " + sessionID
+      let title = "Session " + sessionID
 
-        chrome.bookmarks.create(
-          {'parentId': workingFolderId, 'title': title},
-          function(folder) {
-            console.log("Created new folder for session called: " + folder.title)
-            
-            for (let i = 0; i < data.response.length; i++) {
-              chrome.bookmarks.create({
-                'parentId': folder.id,
-                'title': data.response[i].title,
-                'url': data.response[i].pdfId
-              })
-            }
-          }
-        )
-      }
-    )
+      chrome.bookmarks.create({'parentId': workingFolderId, 'title': title}).then((folder) => {
+        console.log("Created new folder for session called: " + folder.title)
+        
+        for (let i = 0; i < data.response.length; i++) {
+          chrome.bookmarks.create({
+            'parentId': folder.id,
+            'title': data.response[i].title,
+            'url': data.response[i].pdfId
+          })
+        }
+      })
+
+      sessionID++;
+    })
   })
 
-  let test = "hola"
-  chrome.storage.sync.set({ test }).then(() => {
-    console.log("Value is set to " + test);
-  });
+  chrome.storage.sync.get(["amount", "title"]).then((e) => {
+    console.log("amount variable from storage: " + e.amount)
+    console.log("title variable from storage: " + e.title)
 
-  chrome.storage.sync.get(["test"]).then((e) => {
-    console.log("test outpout: " + e.test)
-  })
+    if(e.title != undefined){
+      workingFolderName = e.title
+    } else {
+      workingFolderName = "Papers Found - Chrome Extension"
+    }
 
-  // Check a folder has been created for the paper bookmarks to store to if not create it
-  chrome.bookmarks.getTree(
-    function(bookmarkTree){
+    if(e.amount != undefined){
+      amountOfPapers = e.amount
+    } else {
+      amountOfPapers = 5
+    }
+
+    chrome.bookmarks.getTree().then((bookmarkTree) => {
 
       console.log(bookmarkTree[0].children[0])
-
+  
       bookmarkParent = bookmarkTree[0].children[0]
-
+  
       let folderFound = false
       for (let i = 0; i < bookmarkParent.children.length; i++) {
-        if (bookmarkParent.children[i].title == "Papers Found - Chrome Extension") {
+        if (bookmarkParent.children[i].title == workingFolderName) {
           folderFound = true
           workingFolderId = bookmarkParent.children[i].id
+          
+          for (let j = 0; j < bookmarkParent.children[i].children.length; j++){
+
+            let sessionTitle = bookmarkParent.children[i].children[j].title
+            let sessionNum = sessionTitle[(sessionTitle.length-1)]
+
+            if(sessionID < sessionNum){
+              sessionID = (parseInt(sessionNum) + 1)
+            }
+          }
         }
       }
-
+  
       if(!folderFound) {
-        chrome.bookmarks.create(
-          {'parentId': bookmarkParent.id, 'title': "Papers Found - Chrome Extension"},
-          function(folder) {
-            console.log("New folder created in bookmarks bar: " + folder.title)
-            workingFolderId = folder.id
-            sessionID = 0
-          }
-        )
+        chrome.bookmarks.create({'parentId': bookmarkParent.id, 'title': workingFolderName}).then((folder) => {
+          console.log("New folder created in bookmarks bar: " + folder.title)
+          workingFolderId = folder.id
+          sessionID = 0
+        })
       }
     })
+  })
 
   chrome.contextMenus.create({
       id: "test",
@@ -91,31 +101,50 @@ chrome.runtime.onInstalled.addListener(async () => {
 
 
 
-chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
   console.log("Received: ", request);
 
   switch (request.head){
     case "amount":
-      console.log("amount to change to: " + request.value)
+
+      if(request.value > 10){
+        request.value = 10
+      }
+      if(request < 0){
+        request.value = 1
+      }
       
-      sendResponse({result: true})
+      chrome.storage.sync.set({amount: request.value}).then(() => {
+        console.log("New amount " + request.value + " saved to storage")
+        amountOfPapers = request.value
+        sendResponse({result: true})
+      })
+
       break;
     case "folderRename":
-      console.log("rename folder to: " + request.value)
 
-      chrome.bookmarks.update(workingFolderId, {title: value})
+      chrome.bookmarks.update(workingFolderId, {title: request.value}).then(() => {
+        workingFolderName = request.value
+        console.log("folder renamed to " + request.value)
+        
+        chrome.storage.sync.set({title: request.value}).then((e) => {
+          console.log("New title " + request.value + " saved to storage")
+          sendResponse({result: true})
+        })
+      })
 
-      sendResponse({result: true})
       break;
     default:
       console.log("fell into default")
       sendResponse({result: false})
   }
+
+  return true;
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab){
 
   console.log("info: ")
   console.log(info.selectionText)
-  connection.send(JSON.stringify({body: info.selectionText}))
+  connection.send(JSON.stringify({body: info.selectionText, amount: amountOfPapers}))
 });
